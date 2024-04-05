eb20634350cedd6dbbc121b565471abf2b86f36c
marjo <marjo@DESKTOP-RMIAIB0>
2024-04-05 20:40:30 -0300

    Asistencia Clase practica 05/04/2024

---
+++ asistencia_Practica/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a=8;
+		int b=5;
+		int c=1;
+		int d=20;
+		int e=9;
+		float suma=a+b+c+d+e;
+		float res=suma/5;
+		System.out.println(res);
+	}
+
+}
---
+++ asistencia_Practica/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module asistencia_Practica {
+}
